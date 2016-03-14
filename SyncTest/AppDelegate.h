3499cbe59d1b558c6e0148a7ff5f20e034e7c164
yogesh.rana19@gmail.com <yogesh.rana19@gmail.com>
2016-03-14 21:59:06 +0530

    Initial Commit

---
+++ SyncTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SyncTest
+//
+//  Created by Yogesh Singh Rana on 14/03/16.
+//  Copyright © 2016 Yogesh Singh Rana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SyncTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SyncTest
+//
+//  Created by Yogesh Singh Rana on 14/03/16.
+//  Copyright © 2016 Yogesh Singh Rana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
